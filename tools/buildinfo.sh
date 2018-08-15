RenanQueiroz/build-1

build: Add custom build support
core: Move custom sepolicy to device/

Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

tools/buildinfo.sh
@@ -61,4 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "org.pxielexperience.device=$CUSTOM_DEVICE"
+
 echo "# end build properties"

SilverShades02/build

tools/buildinfo.sh
@@ -61,4 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "org.pixelexperience.device=$CUSTOM_DEVICE"
+
 echo "# end build properties"